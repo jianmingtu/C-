6dfc2f5f9970a7b3b86d6f3bfd103fa8a6c56255
jianming tu <tu_jm@yahoo.com>
2019-04-12 21:00:33 -0700

    test

--- C#_intermediate/Stack/Stack.cs
+++ C#_intermediate/Stack/Stack.cs
@@ -17,7 +17,7 @@ namespace Udemy.Stack
         {
             if (v == null)
             {
-                throw new InvalidOperationException("Push: stack is empty");
+                throw new InvalidOperationException("Push1: stack is empty");
             }
             _stack.Add(v);
         }
@@ -26,7 +26,7 @@ namespace Udemy.Stack
         {
             if (_stack.Count == 0)
             {
-                throw new InvalidOperationException("Pop: stack is empty");
+                throw new InvalidOperationException("Pop1: stack is empty");
             }
 
             var lastIndex = _stack.Count - 1;
